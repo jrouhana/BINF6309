f512830a408be5ad8098f0ffd90439beaa3df57b
jrouhana <rouhana.j@husky.neu.edu>
2017-09-18 19:37:29 -0400

    Put everything into subroutines, added comments

--- SequencingTechnology/qualityTrimmer.pl
+++ SequencingTechnology/qualityTrimmer.pl
@@ -9,35 +9,49 @@ use Bio::Seq::Quality;    # Create sequence read objects
 # Each gets trimmed and made to clear Phred range of 20. The descriptions
 # are retrieved, and an alternating left-right output is written.
 
-# Read in left sequences
-my $left = Bio::SeqIO->new(
-	-file   => "Sample.R1.fastq",
-	-format => 'fastq'
-);
+fastqReadSequences();
 
-# Read in right sequences
-my $right = Bio::SeqIO->new(
-	-file   => "Sample.R2.fastq",
-	-format => 'fastq'
-);
+# Subroutine to read in left and right fastq sequences
+sub fastqReadSequences {
 
-# Object to write interleaved output
-my $interleavedOutput = Bio::SeqIO->new(
-	-file   => ">Interleaved.fastq",
-	-format => 'fastq'
-);
+	my $leftFile = "Sample.R1.fastq";
+	my $rightFile = "Sample.R2.fastq";
 
-while ( my ( $leftFastq, $rightFastq ) =
-	( $left->next_seq(), $right->next_seq() ) )
-{
-	# Put through each Fastq sequence that clears a Phred range of 20
-	my $leftTrimmed = $leftFastq->get_clear_range(20);
-	$leftTrimmed->desc( $leftFastq->desc() ); # Retrieve left description (bug?)
-	# Same for right
-	my $rightTrimmed = $rightFastq->get_clear_range(20);
-	$rightTrimmed->desc( $rightFastq->desc() );    # Retrieve right description
+	# Read in left sequences
+	my $left = Bio::SeqIO->new(
+		-file   => $leftFile,
+		-format => 'fastq'
+	);
 
-	# Write output
-	$interleavedOutput->write_seq($leftTrimmed);
-	$interleavedOutput->write_seq($rightTrimmed);
+	# Read in right sequences
+	my $right = Bio::SeqIO->new(
+		-file   => $rightFile,
+		-format => 'fastq'
+	);
+	alternateSequences($left,$right);
+}
+
+# Subroutine to write left and right sequences in alternating order
+sub alternateSequences {
+	my ($left, $right) = @_;
+	# Object to write interleaved output
+	my $interleavedOutput = Bio::SeqIO->new(
+		-file   => ">Interleaved.fastq",
+		-format => 'fastq'
+	);
+
+	while ( my ( $leftFastq, $rightFastq ) =
+		( $left->next_seq(), $right->next_seq() ) )
+	{
+		# Put through each Fastq sequence that clears a Phred range of 20
+		my $leftTrimmed = $leftFastq->get_clear_range(20);
+		$leftTrimmed->desc( $leftFastq->desc() );    # Retrieve left description 
+		# Same for right
+		my $rightTrimmed = $rightFastq->get_clear_range(20);
+		$rightTrimmed->desc( $rightFastq->desc() ); # Retrieve right description
+
+		# Write output
+		$interleavedOutput->write_seq($leftTrimmed);
+		$interleavedOutput->write_seq($rightTrimmed);
+	}
 }
