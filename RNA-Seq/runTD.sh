b468ee988ad33e26e16e9b870688879f8f9c8ebc
jrouhana <rouhana.j@husky.neu.edu>
2017-10-23 17:50:55 -0400

    gitignore update and shell scripts

---
+++ RNA-Seq/runTD.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# TransDecoder
+nice -n19 /usr/local/programs/TransDecoder-5.0.1/\
+TransDecoder.LongOrfs -t Trinity-GG.fasta
+nice -n19 /usr/local/programs/TransDecoder-5.0.1/\
+TransDecoder.Predict -t Trinity-GG.fasta
+sed -i 's/\*//g' Trinity-GG.fasta.transdecoder.pep
