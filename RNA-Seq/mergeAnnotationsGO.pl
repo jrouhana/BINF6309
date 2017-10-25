1402bc3b8538b8b38ff082739d3fad849e13c755
jrouhana <rouhana.j@husky.neu.edu>
2017-10-25 19:24:14 -0400

    MergeAnnotationsGo.pl now inputs aipSwissProt and pulls out Trinity and protein description data

--- RNA-Seq/mergeAnnotationsGO.pl
+++ RNA-Seq/mergeAnnotationsGO.pl
@@ -3,21 +3,30 @@ use warnings;
 use strict;
 
 # filehandle for input
-open ( SP_TO_GO, "<", "spToGo.tsv" ) or die #!;
+open( SP_TO_GO, "<", "spToGo.tsv" ) or die    #!;
 
-# hash to store swiss prot IDs and Go-Terms
-my %spToGo;
+  # hash to store swiss prot IDs and Go-Terms
+  my %spToGo;
 
 # Loop through input to get swissprot and GO terms
 while (<SP_TO_GO>) {
 	chomp;
-	my ( $swissProt, $go ) = split( "\t", $_);
+	my ( $swissProt, $go ) = split( "\t", $_ );
 	$spToGo{$swissProt}{$go}++;
 }
 
-# testing hash so far
-foreach my $swissProt ( sort keys %spToGo ) {
-	foreach my $go (sort keys %{$spToGo{$swissProt}}){
-		print join ( "\t", $swissProt, $go), "\n";
+# New filehandle for trinity data
+open( SP, "<", "aipSwissProt.tsv" ) or die $!;
+
+while (<SP>) {
+	chomp;
+
+	# print trinity data with protein description, protein name, and GO Term
+	my ( $trinity, $swissProt, $description, $eValue ) =
+	  split( "\t", $_ );
+	if ( defined $spToGo{$swissProt} ) {
+		foreach my $go ( sort keys %{ $spToGo{$swissProt} } ) {
+			print join( "\t", $trinity, $description, $swissProt, $go ), "\n";
+		}
 	}
 }
