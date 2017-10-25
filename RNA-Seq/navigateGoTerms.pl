6a376812b98120344e779e1db4fabbd231aaf42a
jrouhana <rouhana.j@husky.neu.edu>
2017-10-25 19:44:54 -0400

    Added navigateGoTerms.pl to build GP:Hierarchy and print names & identifiers

---
+++ RNA-Seq/navigateGoTerms.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+
+use Bio::OntologyIO;
+
+# Input .obo file for input
+my $parser = Bio::OntologyIO->new(
+	-format => "obo",
+	-file   => "go-basic.obo"
+);
+
+# Loop through ontology to return top-level terms
+while ( my $ont = $parser->next_ontology() ) {
+	print "read ontology", $ont->name(), "with",
+	  scalar( $ont->get_root_terms ), "root terms, and ",
+	  scalar( $ont->get_leaf_terms ), "leaf terms\n";
+
+}
+
+
+while ( my $ont = $parser->next_ontology() ) {
+	# For Aptasia, the most useful GO terms are the
+	# biological process terms. Iterate over them and
+	# print name and identifier
+	if ( $ont->name() eq "biological_process" ) {
+		foreach my $leaf ( $ont->get_leaf_terms ) {
+			my $go_name = $leaf->name();
+			my $go_id   = $leaf->identifier();
+			print join( "\t", $go_id, $go_name ), "\n";
+		}
+	}
+}
